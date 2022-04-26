7fe33f7567abaf02d484c5eace5548d17141b62e
17felix <19fmysko@gmail.com>
2022-04-26 16:31:09 +0300

    refactor

--- testApi/Program.cs
+++ testApi/Program.cs
@@ -5,9 +5,7 @@ using testApi;
 
 HttpClient client = new HttpClient();
 var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwibmFtZSI6InN0cmluZyIsInJvbGUiOlsiQWRtaW4iLCJVc2VyIl0sIm5iZiI6MTY1MDk3NzEyMiwiZXhwIjoxNjUwOTkxNTIyLCJpYXQiOjE2NTA5NzcxMjJ9._HQjWXA19KIEwpiThGgsdPw6CgxTsMdcCEyqBZ98KDE";
-client.DefaultRequestHeaders.Authorization =
-    new AuthenticationHeaderValue("Bearer", $"{token}");
-Console.WriteLine(token.GetType());
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{token}");
 
 try
 {
@@ -17,11 +15,6 @@ try
     statusesResponse.EnsureSuccessStatusCode();
     string loginUserResponseBody = await loginUserResponse.Content.ReadAsStringAsync();
     string StatusesResponseBody = await statusesResponse.Content.ReadAsStringAsync();
-    // Above three lines can be replaced with new helper method below
-    // Above three lines can be replaced with new helper method below
-    // string responseBody = await client.GetStringAsync(uri);
-    //Newtonsoft.Json.Linq.JObject.Parse(loginUserResponseBody);
-    //Newtonsoft.Json.Linq.JArray.Parse(StatusesResponseBody);
 
     var statuses = JsonConvert.DeserializeObject<ICollection<StatusDTO>>(StatusesResponseBody);
     var login2 = JsonConvert.DeserializeObject<LoginUserDTO>(loginUserResponseBody);
@@ -38,19 +31,3 @@ catch (HttpRequestException e)
     Console.WriteLine("\nException Caught!");
     Console.WriteLine("Message :{0} ", e.Message);
 }
-
-string json = @"{
-  'Email': 'james@example.com',
-  'Active': true,
-  'CreatedDate': '2013-01-20T00:00:00Z',
-  'Roles': [
-    'User',
-    'Admin'
-  ]
-}";
-
-LoginUserDTO login = JsonConvert.DeserializeObject<LoginUserDTO>(json);
-//StatusDTO status = JsonConvert.DeserializeObject<StatusDTO>(StatusesResponseBody);
-//StatusDTO statusSerialize = JsonSerializer.DeserializeAsync<StatusDTO>(await json);
-Console.WriteLine(login.Bara);
-// james@example.com
