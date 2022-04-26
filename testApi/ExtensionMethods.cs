5f58b4b50865f05c57a5a673ca40758e9dfca660
17felix <19fmysko@gmail.com>
2022-04-26 16:54:49 +0300

    refactore with extension method

---
+++ testApi/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testApi
+{
+    public static class ExtensionMethods
+    {
+        public static async Task<string> GetClientRespnose(this HttpClient client, string uri)
+        {
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            response.EnsureSuccessStatusCode();
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+    }
+}
--- testApi/Program.cs
+++ testApi/Program.cs
@@ -6,25 +6,24 @@ using testApi;
 HttpClient client = new HttpClient();
 var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwibmFtZSI6InN0cmluZyIsInJvbGUiOlsiQWRtaW4iLCJVc2VyIl0sIm5iZiI6MTY1MDk3NzEyMiwiZXhwIjoxNjUwOTkxNTIyLCJpYXQiOjE2NTA5NzcxMjJ9._HQjWXA19KIEwpiThGgsdPw6CgxTsMdcCEyqBZ98KDE";
 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", $"{token}");
+var uriLogin = "https://localhost:7115/UsersControllercs/login?userName=string&password=string.123A";
+var uriStatus = "https://localhost:7115/statuses";
 
 try
 {
-    HttpResponseMessage loginUserResponse = await client.GetAsync("https://localhost:7115/UsersControllercs/login?userName=string&password=string.123A");
-    HttpResponseMessage statusesResponse = await client.GetAsync("https://localhost:7115/statuses");
-    loginUserResponse.EnsureSuccessStatusCode();
-    statusesResponse.EnsureSuccessStatusCode();
-    string loginUserResponseBody = await loginUserResponse.Content.ReadAsStringAsync();
-    string StatusesResponseBody = await statusesResponse.Content.ReadAsStringAsync();
+    var loginUserResponseBody = await ExtensionMethods.GetClientRespnose(client, uriLogin);
+    var statusesResponseBody = await ExtensionMethods.GetClientRespnose(client, uriStatus);
 
-    var statuses = JsonConvert.DeserializeObject<ICollection<StatusDTO>>(StatusesResponseBody);
-    var login2 = JsonConvert.DeserializeObject<LoginUserDTO>(loginUserResponseBody);
+    var statuses = JsonConvert.DeserializeObject<ICollection<StatusDTO>>(statusesResponseBody);
+    var login = JsonConvert.DeserializeObject<LoginUserDTO>(loginUserResponseBody);
+
     Console.WriteLine(loginUserResponseBody);
-    Console.WriteLine(StatusesResponseBody);
+    Console.WriteLine(statusesResponseBody);
     foreach (var status in statuses)
     {
         Console.WriteLine(status.Id + " " + status.Name);
     }
-    Console.WriteLine(login2.Bara);
+    Console.WriteLine(login.Bara);
 }
 catch (HttpRequestException e)
 {
